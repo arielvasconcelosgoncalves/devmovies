747e1af16f7eb424170900efc6641774c2812a2d
arielvasconcelosgoncalves <ariel.goncalves@sou.unifeob.edu.br>
2025-11-07 14:13:17 -0300

    update: adicionado o DefaultLayout para incluir o Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import Logo from '../../assets/logo.png';
+
+const Header = () => {
+  return (
+    <div>
+      <img src={Logo} alt="Logo do DevMovies" />
+    </div>
+  );
+};
+
+export default Header;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,13 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import Movies from '../pages/Movies';
 import Series from '../pages/Series';
+import DefaultLayout from '../layout/DefaultLayout';
 
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/movies" element={<Movies />}></Route>
-      <Route path="/series" element={<Series />}></Route>
+      <Route element={<DefaultLayout />}>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movies" element={<Movies />}></Route>
+        <Route path="/series" element={<Series />}></Route>
+      </Route>
     </Routes>
   );
 };
